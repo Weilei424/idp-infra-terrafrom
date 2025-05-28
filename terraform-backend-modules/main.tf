38d033c0f973e9656cf725a70467d51b8c34d9e2
Mason Wang <mason123wp@gmail.com>
2025-05-28 19:28:37 -0400

    Applied

--- terraform-backend-modules/main.tf
+++ terraform-backend-modules/main.tf
@@ -1,1 +1,19 @@
-# placeholder
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "idp-terraform-state"
+  force_destroy = true
+}
+
+resource "aws_dynamodb_table" "tf_locks" {
+  name         = "idp-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
